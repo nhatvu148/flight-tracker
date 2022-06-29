47d179f1dddbedca97a18af12c287b2f9ee7764f
Nhật Vũ <nhatvu148@gmail.com>
2022-06-29 15:08:08 +0900

    comment getServerSideProps

--- pages/[coordinates]/[zoom].tsx
+++ pages/[coordinates]/[zoom].tsx
@@ -42,26 +42,26 @@ const Zoom: FC<IProps> = ({ main: { mapCenter, zoom } }) => {
 };
 
 //// Loading large data from server side may cause issue in production --> Use client side query or prefetch the first few data
-export const getServerSideProps: GetServerSideProps = async ({
-  params,
-  res,
-}) => {
-  // const { coordinates, zoom } = params;
-  // console.log({ coordinates, zoom });
-  res.setHeader("Cache-Control", `s-maxage=60, stale-while-revalidate`);
+// export const getServerSideProps: GetServerSideProps = async ({
+//   params,
+//   res,
+// }) => {
+//   // const { coordinates, zoom } = params;
+//   // console.log({ coordinates, zoom });
+//   res.setHeader("Cache-Control", `s-maxage=60, stale-while-revalidate`);
 
-  const queryClient = new QueryClient();
+//   const queryClient = new QueryClient();
 
-  // prefetch the first 100 results
-  await queryClient.prefetchQuery("flights", () => getFlights(100));
-  await queryClient.prefetchQuery("airports", () => getAirports(100));
+//   // prefetch the first 100 results
+//   await queryClient.prefetchQuery("flights", () => getFlights(100));
+//   await queryClient.prefetchQuery("airports", () => getAirports(100));
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
 
 const mapStateToProps = (state: IAppState): IStateProps => ({
   main: getMain(state),
