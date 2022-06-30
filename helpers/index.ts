44b89d44cfe78b9948f224fae79c3d395304e469
Nhật Vũ <nhatvu148@gmail.com>
2022-06-30 12:54:39 +0900

    update helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -172,12 +172,6 @@ export const layerMap = (publicRuntimeConfig: any) => [
     url: "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png",
   },
   {
-    name: "Outdoors",
-    attribution:
-      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    url: "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png",
-  },
-  {
     name: "OpenTopoMap",
     attribution:
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
