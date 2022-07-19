8964db9cdf6dc52f2f1db9bd2f6f35729bcbb367
Nhật Vũ <nhatvu148@gmail.com>
2022-07-19 11:29:34 +0900

    proxy rewrites

--- next.config.js
+++ next.config.js
@@ -1,19 +1,31 @@
-module.exports = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: "secret",
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: "/static",
-    mapboxToken: process.env.MAPBOX_TOKEN, // Pass through env variables
-    mapTilerToken: process.env.MAPTILER_TOKEN,
-    aviationToken: process.env.AVIATION_TOKEN,
-    mapThunderforestToken: process.env.THUNDERFOREST_TOKEN,
-    apiURL: process.env.API_URL,
-  },
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
+module.exports = () => {
+  const rewrites = async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:55556/api/:path*",
+      },
+    ];
+  };
+
+  return {
+    serverRuntimeConfig: {
+      // Will only be available on the server side
+      mySecret: "secret",
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      staticFolder: "/static",
+      mapboxToken: process.env.MAPBOX_TOKEN, // Pass through env variables
+      mapTilerToken: process.env.MAPTILER_TOKEN,
+      aviationToken: process.env.AVIATION_TOKEN,
+      mapThunderforestToken: process.env.THUNDERFOREST_TOKEN,
+      apiURL: process.env.API_URL,
+    },
+    images: {
+      loader: "akamai",
+      path: "/",
+    },
+    rewrites,
+  };
 };
